cde35f50c01043c21abab1d6a7d39fe8bd7764b3
Roman <bardlesswk@gmail.com>
2019-12-09 22:21:53 +0200

    reception sporthall

---
+++ Reception/Persons.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reception
+{
+    public class People
+    {
+        public static List<Persons> persons = new List<Persons>();
+        public static void Add(Persons p)
+        {
+            persons.Add(p);
+        }
+
+        public static void Remove(Persons p)
+        {
+            persons.Remove(p);
+        }
+
+        public static void Print()
+        {
+            if (persons.Any())
+            {
+                foreach (var name in persons)
+                {
+                    Console.WriteLine(name.ToString() + " Yet dooooolgo!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nobody in the hall");
+            }
+        }
+        public static int Gym(int i) 
+        {
+            Console.WriteLine("Doing execise");
+            i++;
+            return i;
+        }
+        public class Persons
+        {
+           public Persons(string Name, int kolpod)
+            {
+                this.Name = Name;
+                this.kolpod = kolpod;
+            }
+            public string Name { get; set; }
+            public int kolpod { get; set; }
+            public override string ToString()
+            {
+                return $"Name = {Name}, kolpod = {kolpod}";
+            }
+        }
+    }
+}
---
+++ Reception/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reception
+{
+    class Program
+    {
+        //public List<Persons> persons = new List<Persons>();
+        static void Main(string[] args)
+        {
+            string name;
+            int kol;
+            int kol_fin_pod=0;
+
+            int vibormenu = 0;
+            int znachmenu;
+            while (!(vibormenu==4))
+            {
+                Console.WriteLine("Menu:\n1 - Add\n2 - Display\n3 - Do gym\n4 - Close");
+                znachmenu = int.Parse(Console.ReadLine());
+                switch (znachmenu) 
+                {
+                    case 1:
+                        Console.WriteLine("Input Name");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Input kol-vo gym");
+                        //kol = int.Parse(Console.ReadLine());
+                        kol = ProverkaVvoda();
+                        var p = new People.Persons(name, kol);
+                        People.Add(p);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        People.Print();
+                        break;
+                    case 3:
+                        kol_fin_pod = People.Gym(kol_fin_pod);
+
+                        if (kol_fin_pod > 0)
+                        {
+                            foreach (var v in People.persons) v.kolpod = v.kolpod - kol_fin_pod;
+                            kol_fin_pod = 0;
+                        }
+
+                        foreach (var z in People.persons)
+                        {
+                            if (z.kolpod <= 0)
+                            {
+                                Console.WriteLine("{0} finished training", z.Name);
+                                Reception.People.persons.Remove(z);
+                                break;
+                            }
+                        }
+                        break;
+                    case 4:
+                        vibormenu = znachmenu;
+                        break;
+                }
+            }
+        }
+
+        static int ProverkaVvoda() 
+        {
+            int i;
+            while (!int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine("Input coorect value");
+            }
+            return i;
+        }
+    }
+}
